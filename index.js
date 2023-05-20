f78e7a0ebdf359b46f973f8c0532c6e6248498a9
Sanatan Swain <99419569+sanatans@users.noreply.github.com>
2023-05-20 20:27:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const {
-  applyExtends,
-  cjsPlatformShim,
-  Parser,
-  processArgv,
-} = require('../build/index.cjs');
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import myReducers from "./context/reducers";
 
-module.exports = {
-  applyExtends: (config, cwd, mergeExtends) => {
-    return applyExtends(config, cwd, mergeExtends, cjsPlatformShim);
-  },
-  hideBin: processArgv.hideBin,
-  Parser,
-};
+const myStore = createStore(
+  myReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <AnimatePresence>
+        <Provider store={myStore}>
+          <App />
+        </Provider>
+      </AnimatePresence>
+    </Router>
+  </React.StrictMode>
+);
